e17e79020a8e7704dd58496269b3be5b93c3696c
Chetan Kalia <ckalia@uwaterloo.ca>
2023-06-03 18:08:45 -0400

    Delete django_challenge directory

--- django_challenge/videos_interface/context_processors.py
+++
@@ -1,5 +0,0 @@
-from .forms import VideoSearchForm
-
-def video_search_form(request):
-    form = VideoSearchForm()
-    return {'video_search_form': form}
--- django_challenge/videos_interface/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def truncate_chars(value, max_length):
-    if len(value) > max_length:
-        return value[:max_length] + '...'
-    return value
