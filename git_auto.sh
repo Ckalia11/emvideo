f590cc97c9267d340e90dfc63f2b3dc3635405ec
Chetan <ckalia@uwaterloo.ca>
2024-01-27 14:22:59 -0500

    Auto commit

---
+++ git_auto.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if there are any changes to commit
+if [[ -n $(git status -s) ]]; then
+    # Add all changes
+    git add .
+
+    # Commit with a default message or you can customize it
+    git commit -m "Auto commit"
+
+    # Push to the remote repository
+    git push
+else
+    echo "No changes to commit."
+fi
--- videos_interface/models.py
+++ videos_interface/models.py
@@ -4,14 +4,10 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# Create your models here.
-# should have a user table
 
 class Video(models.Model):
     title = models.CharField(max_length=200, blank=True)
-    videofile= models.FileField(upload_to='videos/', blank=True)
-    clicks = models.IntegerField(null=True, blank=True)
-    views = models.IntegerField(null=True, blank = True)
+    videofile = models.FileField(upload_to='videos/', blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -19,36 +15,29 @@ class Video(models.Model):
     def __str__(self):
         return f"Video {self.pk}"
 
+
 class Comment(models.Model):
     comment = models.CharField(max_length=100)
-    user =  models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     video = models.ForeignKey(Video, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"Comment {self.pk}"
 
+
 class Thumbnail(models.Model):
-    image_file = models.ImageField(upload_to = "images/thumbnails/")
+    image_file = models.ImageField(upload_to="images/thumbnails/")
     video = models.OneToOneField(
         Video,
-        blank = True,
-        null = True,
+        blank=True,
+        null=True,
         on_delete=models.CASCADE,
     )
 
     def __str__(self):
         return f"Thumbnail {self.pk}"
 
+
 class Channel(models.Model):
-    image = models.ImageField(null = True)
-    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
-
- # class Clicks(models.Model):
-#     clicks = models.IntegerField()
-#     video = models.ForeignKey(Video, on_delete=models.CASCADE, null=True, related_name='click_video')
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
-#     class Meta:
-#         verbose_name = 'Click'
-#         verbose_name_plural = 'Clicks'
-
+    image = models.ImageField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
